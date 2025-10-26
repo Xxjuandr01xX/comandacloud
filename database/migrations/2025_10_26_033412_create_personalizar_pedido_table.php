85fa00144cea7f7ae0ac2ebd65808086570ee948
Juan D Rincon U <jd.rincon021@gmail.com>
2025-10-25 23:36:30 -0400

    creacion de migraciones esenciales de la base de datos

---
+++ database/migrations/2025_10_26_033412_create_personalizar_pedido_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personalizar_pedido', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained('pedidos')->cascadeOnDelete();
+            $table->foreignId('producto_id')->constrained('productos')->cascadeOnDelete();
+            $table->foreignId('ingrediente_id')->constrained('ingredientes_productos')->cascadeOnDelete();
+            $table->foreignId('adicional_id')->constrained('adicionales_productos')->cascadeOnDelete()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personalizar_pedido');
+    }
+};
